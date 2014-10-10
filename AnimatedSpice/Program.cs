f63e4b63e40a7873725169c47d11bce2cdf4ccae
Bill DeLude <qanwi1970@gmail.com>
2014-10-10 12:51:20 -0600

    Initial checkin

---
+++ AnimatedSpice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using AnimatedSpice.DAL;
+
+namespace AnimatedSpice
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            using (var dbContext = new SchoolContext())
+            {
+                foreach (var student in dbContext.Students)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", student.ID, student.FirstMidName, student.LastName, student.EnrollmentDate);
+                }
+            }
+
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
